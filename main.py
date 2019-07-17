ce97e7d9ab6397d58d002f66a94016357152f861
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-07-17 17:49:11 +0800

    Checking for case where stack might be empty

--- main.py
+++ main.py
@@ -21,17 +21,10 @@ tables = document.tables
 while len(imageList) != 0:
     run = document.add_paragraph().add_run()
     print('adding 2 pictures')
-    run.add_picture(imageList.pop(0), width=Inches(3))
-    run.add_picture(imageList.pop(0), width=Inches(3))
-    #create a 1x2 table
-    #table = document.add_table(rows=1, cols=2)
-    #insert 2 pictures
-    #for row in table.rows:
-        #for cell in row.cells:
-            #paragraph = cell.paragraphs[0]
-            #run = paragraph.add_run()
-            #print('adding picture...')
-            #run.add_picture(imageList.pop(0), width=Inches(3))
+    if len(imageList) != 0:
+        run.add_picture(imageList.pop(0), width=Inches(3))
+    if len(imageList) != 0:
+        run.add_picture(imageList.pop(0), width=Inches(3))
 
 #save the doc
 document.save('C:/Users/user/Documents/FCA/Blueprints/L2_bp.docx')
